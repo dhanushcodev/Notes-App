898fb54ce82caca59b07d9a6fec55de4b4caf334
dhanushcodev <kdhanush894@gmail.com>
2024-06-27 00:19:31 +0530

    versionName and versionCode changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.notes"
         minSdk = 28
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 0
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
